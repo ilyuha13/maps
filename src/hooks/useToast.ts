28a5118962b0f3fad303a1ae61fe0e83d6b2845b
Ilya Churshukov <ilyachurshukov13@gmail.com>
2025-12-04 10:51:54 +0300

    fix: delete exsces useMemo on useToast

--- src/hooks/useToast.ts
+++ src/hooks/useToast.ts
@@ -1,26 +1,23 @@
-import { useContext, useMemo } from "react";
+import { useContext } from "react";
 import { ToastContext } from "../context/ToastContext";
 
-export function useToast() {
+export const useToast = () => {
   const context = useContext(ToastContext);
 
   if (!context) {
     throw new Error("useToast must be used within ToastProvider");
   }
 
-  return useMemo(
-    () => ({
-      success: (message: string, duration?: number) => {
-        context.addToast("success", message, duration);
-      },
-      error: (message: string, duration?: number) => {
-        context.addToast("error", message, duration);
-      },
-      info: (message: string, duration?: number) => {
-        context.addToast("info", message, duration);
-      },
-      remove: context.removeToast,
-    }),
-    [context]
-  );
-}
+  return {
+    success: (message: string, duration?: number) => {
+      context.addToast("success", message, duration);
+    },
+    error: (message: string, duration?: number) => {
+      context.addToast("error", message, duration);
+    },
+    info: (message: string, duration?: number) => {
+      context.addToast("info", message, duration);
+    },
+    remove: context.removeToast,
+  };
+};
